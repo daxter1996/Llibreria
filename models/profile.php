8fb0057d727608be8fe02fb1f4b0846e4524d911
Jesus <jesuslizalles@gmail.com>
2017-02-22 09:10:54 +0100

    Refactor Molt gros

--- models/profile.php
+++ models/profile.php
@@ -13,6 +13,7 @@ if(isset($_POST)){
     echo $_SESSION["user_id"]->$_POST["action"]($_POST["name"],$_POST["surname"],$_POST["dni"],$_POST["address"]);
     if (empty($_FILES)){
         echo uploadPhoto();
+
     }
 }
 
