3380a793d4cbf9d060a446fbec2cb6f7d6f4e573
daxter1996 <jesuslizalles@gmail.com>
2017-03-15 12:18:41 +0100

    qwe

--- models/profileModel.php
+++ models/profileModel.php
@@ -1,31 +1,32 @@
 <?php
-require_once(dirname(__DIR__)."/classes/libraryUtility.php");
+require_once(dirname(__DIR__) . "/classes/libraryUtility.php");
 if (isset($_COOKIE["PHPSESSID"])) {
     session_start();
 }
 
 /*Dynamic call*/
 
-if (isset($_POST["controller"]) && $_POST["controller"]== "session") {
+if (isset($_POST["controller"]) && $_POST["controller"] == "session") {
     echo $_SESSION["user_id"]->{$_POST["action"]}($_POST["name"], $_POST["surname"], $_POST["address"]);
     if ($_FILES["profilePhoto"]["error"] != 4) {
         echo uploadPhoto();
     }
-}else{
+} else {
     echo $_POST["action"]();
 }
 
 
-function deleteAcc(){
+function deleteAcc()
+{
     $db = new DB();
-    $sql = "delete from user  where email = '".$_SESSION["user_id"]->getEmail()."'";
-    if ($_POST["email"] == $_SESSION["user_id"]->getEmail()){
-        if ($db->insertBd($sql)){
+    $sql = "delete from user  where email = '" . $_SESSION["user_id"]->getEmail() . "'";
+    if ($_POST["email"] == $_SESSION["user_id"]->getEmail()) {
+        if ($db->insertBd($sql)) {
             return "Account Deleted :(";
-        }else{
+        } else {
             return "Error";
         }
-    }else{
+    } else {
         return "Incorrect Email";
     }
 }
@@ -35,13 +36,18 @@ function uploadPhoto()
     $check = getimagesize($_FILES["profilePhoto"]["tmp_name"]);
     $target_dir = dirname(__DIR__) . "/img/profile/";
 
-    $fileName = explode('.',  $_FILES['profilePhoto']['name']);
+    $fileName = explode('.', $_FILES['profilePhoto']['name']);
     $fileName = end($fileName);
     $fileName = strtolower($fileName);
 
+    $glob = glob($target_dir . "profile_" . $_SESSION["user_id"]->getId() . '.*');
+    if (!empty($glob)) {
+        unlink($glob[0]);
+    }
     if ($check !== false) {
         $target_file_content = $target_dir . "profile_" . $_SESSION["user_id"]->getId() . '.' . $fileName;
-        chmod($target_file_content, 0777);
+
+        /*chmod($target_file_content, 0777);*/
         if (file_exists($target_file_content)) {
             unlink($target_file_content);
         }
